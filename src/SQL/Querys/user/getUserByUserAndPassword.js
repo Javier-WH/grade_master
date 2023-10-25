c2ac378b9b2e8f666eaa6f30dfe67e7429eeebea
Francisco Javier Rodríguez Hernández <fj_rh@hotmail.com>
2023-10-25 16:38:33 -0400

    auto

--- src/SQL/Querys/user/getUserByUserAndPassword.js
+++ src/SQL/Querys/user/getUserByUserAndPassword.js
@@ -1,6 +1,4 @@
 import User from '../../../models/user/user.js'
-import { AuthenticationError } from '../../../errors/authentication_errors.js'
-import getToken from '../../../utils/tokenMaker.js'
 
 export default async function getUserByUserAndPassword ({ user, password }) {
   const userData = await User.findOne(
@@ -11,15 +9,5 @@ export default async function getUserByUserAndPassword ({ user, password }) {
       }
     }
   )
-
-  if (userData === null) {
-    throw new AuthenticationError()
-  }
-
-  const token = getToken(userData)
-
-  return {
-    id: userData.id,
-    Authorization: token
-  }
+  return userData
 }
--- src/controllers/user/login.js
+++ src/controllers/user/login.js
@@ -1,10 +1,22 @@
 import getUserByUserAndPassword from '../../SQL/Querys/user/getUserByUserAndPassword.js'
 import ErrorHandler from '../../errors/errorHandler.js'
+import getToken from '../../utils/tokenMaker.js'
+import { AuthenticationError } from '../../errors/authentication_errors.js'
 
 export default async function login (req, res) {
   try {
     const user = await getUserByUserAndPassword(req.body)
-    res.status(200).json(user)
+    if (user === null) {
+      throw new AuthenticationError()
+    }
+
+    const Authorization = getToken(user)
+    const { id } = user
+    const userData = {
+      id,
+      Authorization
+    }
+    res.status(200).json(userData)
   } catch (error) {
     const { message, code } = ErrorHandler(error)
     res.status(code).send(message)
