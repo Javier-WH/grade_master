527e97b67f1eeb20862f0ebcbe6f09ead8cd1ccd
Francisco Javier Rodríguez Hernández <fj_rh@hotmail.com>
2023-10-27 18:27:50 -0400

    getseccionBySubjectid added

--- src/SQL/Querys/seccions/getStudentsBySubjectId.js
+++ src/SQL/Querys/seccions/getStudentsBySubjectId.js
@@ -22,13 +22,25 @@ export default async function getStudentsBySubjectId (seccionId) {
     'failed.subject7,' +
     'failed.subject8,' +
     'failed.subject9,' +
-    'failed.subject10 ' +
+    'failed.subject10, ' +
+    'grades.eval1, ' +
+    'grades.eval2, ' +
+    'grades.eval3, ' +
+    'grades.eval4, ' +
+    'grades.eval5, ' +
+    'grades.eval6, ' +
+    'grades.eval7, ' +
+    'grades.eval8, ' +
+    'grades.eval9, ' +
+    'grades.eval10 ' +
     'FROM students ' +
     'JOIN subjects ON students.idSeccion = subjects.idSeccion ' +
     'JOIN subjectsnames ON subjects.idSubjectName = subjectsnames.id ' +
     'JOIN seccions ON subjects.idSeccion = seccions.id ' +
     'JOIN seccionsnames ON seccions.idSeccionName = seccionsnames.id ' +
     'JOIN users ON subjects.idUser = users.id ' +
+    'LEFT JOIN evaluationplan ON evaluationplan.idSubject = subjects.id ' +
+    'LEFT JOIN grades ON grades.idEvaluationPlan = evaluationplan.id AND grades.idStudent = students.id ' +
     'LEFT JOIN failed ON students.failed = failed.id ' +
     `WHERE subjects.id = '${seccionId}'`
 
