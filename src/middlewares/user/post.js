8f2c4616b938a8b7b00629666d1cc830c25ffd26
Francisco Javier Rodríguez Hernández <fj_rh@hotmail.com>
2023-12-05 09:50:23 -0400

    update password fuction added

--- src/middlewares/user/post.js
+++ src/middlewares/user/post.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import validateUserData from '../../validators/user/validateLoginData.js'
+import validateUpdateUserPassword from '../../validators/user/validateUpdatePasswordData.js'
 import ErrorHandler from '../../errors/errorHandler.js'
 import { MissingDataError } from '../../errors/authentication_errors.js'
 const router = express.Router()
@@ -18,7 +19,9 @@ function validate (req, res, next) {
   }
 }
 
-router.post('/login', express.json(), validate)
+router.post('/login', express.json(), (req, res, next) => {
+  next()
+})
 
 router.post('/signup', express.json(), validate)
 
@@ -35,6 +38,16 @@ router.post('/updateUserData', express.json(), (req, res, next) => {
 })
 
 router.post('/updateUserPass', express.json(), (req, res, next) => {
-  next()
+  try {
+    const { error, value } = validateUpdateUserPassword(req.body)
+    if (error) {
+      throw new MissingDataError(error.message)
+    }
+    req.body = value
+    next()
+  } catch (error) {
+    const { code, message } = ErrorHandler(error)
+    res.status(code).send(message)
+  }
 })
 export default router
